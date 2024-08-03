8df9fcbb76d69f3c4ea53366a67d0e911b508302
nonlux <nonluxi@gmail.com>
2024-08-04 02:33:18 +0300

    tests

--- @nonlux/fetcher/jest.config.js
+++ @nonlux/fetcher/jest.config.js
@@ -6,7 +6,7 @@ exports.default = {
     preset: '../../jest.preset.js',
     testEnvironment: 'node',
     transform: {
-        '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+      '^.+\\.[tj]s$': ['@swc/jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
     },
     moduleFileExtensions: ['ts', 'js', 'html'],
     coverageDirectory: '../../coverage/@nonlux/fetcher',
--- @nonlux/fetcher/jest.config.ts
+++ @nonlux/fetcher/jest.config.ts
@@ -4,8 +4,16 @@ export default {
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+    '^.+\\.[tj]s$': [
+      '@swc/jest',
+      {
+        //tsconfig: '<rootDir>/tsconfig.spec.json',
+        exclude: [],
+      },
+    ],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/@nonlux/fetcher',
 };
+
+//"exclude": ["jest.config.ts",".*\\.spec.tsx?$",".*\\.test.tsx?$","./src/jest-setup.ts$","./**/jest-setup.ts$",".*.js$"]
